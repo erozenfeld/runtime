d9c4514aca3f79a6a0c5d7a1978b14d6695e3002
Eugene Rozenfeld <erozen@microsoft.com>
2019-09-24 14:27:06 -0700

    Add a simple CallGraph implementation.

---
+++ src/coreclr/src/tools/crossgen2/ILCompiler.ReadyToRun/Compiler/CallGraph.cs
@@ -0,0 +1,80 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+
+using Internal.TypeSystem;
+using ILCompiler.DependencyAnalysis.ReadyToRun;
+
+
+namespace ILCompiler
+{
+    public class CallGraphNode
+    {
+        public CallGraphNode(MethodDesc method)
+        {
+            Method = method;
+        }
+
+        public void AddCallee(CallGraphNode callee)
+        {
+            if (_callees == null)
+            {
+                _callees = new List<CallGraphNode>();
+            }
+
+            _callees.Add(callee);
+        }
+
+        public MethodDesc Method { get; private set; }
+
+        List<CallGraphNode> _callees;
+    }
+
+    public class CallGraph
+    {
+        HashSet<CallGraphNode> _rootNodes;
+        HashSet<CallGraphNode> _nodes;
+        Dictionary<MethodDesc, CallGraphNode> _methodDescToCallGraphNode;
+
+        public CallGraph()
+        {
+            _rootNodes = new HashSet<CallGraphNode>();
+            _nodes = new HashSet<CallGraphNode>();
+            _methodDescToCallGraphNode = new Dictionary<MethodDesc, CallGraphNode>();
+
+        }
+
+        public CallGraphNode AddNode(MethodDesc method)
+        {
+            CallGraphNode node = new CallGraphNode(method);
+            _methodDescToCallGraphNode[method] = node;
+            _nodes.Add(node);
+            return node;
+        }
+
+        public void AddRootNode(MethodDesc method)
+        {
+            CallGraphNode node = AddNode(method);
+            _rootNodes.Add(node);
+        }
+
+        public void AddGraphEdge(MethodDesc caller, MethodDesc callee)
+        {
+            Console.WriteLine("Adding call graph edge from {0} to {1}", caller.ToString(), callee.ToString());
+            CallGraphNode calleeNode;
+            if (!_methodDescToCallGraphNode.TryGetValue(callee, out calleeNode))
+            {
+                calleeNode = new CallGraphNode(callee);
+            }
+            _methodDescToCallGraphNode[caller].AddCallee(calleeNode);
+        }
+
+        public ICollection<CallGraphNode> GetNodes()
+        {
+            return _nodes;
+        }
+    }
+}
